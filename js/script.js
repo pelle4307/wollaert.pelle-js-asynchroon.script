567507ce88f0a6842f7fc54c0580a79371dcb271
pelle4307 <pelwoll7@gmail.com>
2024-10-19 14:09:52 +0200

    Werkende serverophaling aan de hand van knop

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
-function fetchData() {
+function getServerData() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const data = ["Leerling 1", "Leerling 2", "Leerling 3", "..."];
+            const data = ["Senne Evens", "Ymke Huybrechts", "Rens Lettanie", "Matthias Massart", "Loula Siera", "Eli Vanrijkelen", "Pelle Wollaert"];
             resolve(data);
         }, 2000);
     });
 }
+
+document.getElementById('loadButton').addEventListener('click', fetchData);
+
+async function fetchData() {
+    const container = document.querySelector('.container');
+    const ul = document.getElementById('studentListElement');
+    const button = document.getElementById('loadButton');
+
+    container.classList.add('loading');
+
+    const data = await getServerData();
+
+    if (data && data.length > 0) {
+        ul.innerHTML = '';
+
+        for (let i = 0; i < data.length; i++) {
+            const li = document.createElement('li');
+            li.textContent = data[i];
+            ul.appendChild(li);
+        }
+    }
+
+    container.classList.remove('loading');
+}
+
+
+
